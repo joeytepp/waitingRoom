4feec027b67c1388c3f729bdc7fe83f88446e55a
Joey Tepperman <15jmt11@queensu.ca>
2018-02-03 15:08:07 -0500

    a

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var expressHbs = require('express-handlebars');
+//var expressHbs = require('express-handlebars');
 var mongoose = require('mongoose');
 
 var index = require('./routes/index');
