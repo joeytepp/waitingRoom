2c2854102cabbe26d50f87343054c2dd4e3422b3
Joey Tepperman <15jmt11@queensu.ca>
2018-02-03 15:03:32 -0500

    message

--- seed/hospital-seeder.js
+++ seed/hospital-seeder.js
@@ -2,6 +2,7 @@ var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017/";
 var Hospital = require('../models/hospital');
 var fs = require('fs');
+var request = require('request');
 
 var testHospital = new Hospital({
   name: 'Hospital',
@@ -27,7 +28,33 @@ fs.readFile('hospitalList.txt', 'utf8', function(err, data){
         console.log("1 document inserted");
         db.close();
       });
+      getLat(list[i]);
     }
 
   });
 });
+
+var getLat = function(key){
+  console.log("Trying for "+key);
+  var url = "http://maps.googleapis.com/maps/api/geocode/json?address=" + key;
+  request({
+      url: url,
+      json: true
+  }, function (error, response, body) {
+
+      if (!error && response.statusCode === 200) {
+          var results = body.results;
+          try{
+            return results[0].geometry.location.lat;
+          }catch(err){
+            console.log("There was an error");
+          }
+          //console.log(body.results) // Print the json response
+      }
+  });
+  return
+}
+
+var getLong = function(key){
+
+}
