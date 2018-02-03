91a04f64b970d13f42fc6d34b6f3b5bac8aca739
Joey Tepperman <15jmt11@queensu.ca>
2018-02-03 15:06:42 -0500

    message

--- models/hospital.js
+++ models/hospital.js
@@ -4,7 +4,9 @@ var Schema = mongoose.Schema;
 var schema = new Schema({
     name: {type: String, required: true},
     inLine: {type: String, required: true},
-    onTheWay: {type: Number, required: true}
+    onTheWay: {type: Number, required: true},
+    lng: {type: Number, required: true},
+    lat: {type: Number, required: true}
 });
 
 module.exports = mongoose.model('Product', schema);
