e2944d72ffa1417f3c43f703263ef4b7934f273a
Joey Tepperman <15jmt11@queensu.ca>
2018-02-03 03:24:23 -0500

    dropdown

--- routes/index.js
+++ routes/index.js
@@ -8,18 +8,6 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'WaitingRoom' });
 });
 
-var url = "http://developer.cumtd.com/api/v2.2/json/GetStop?" +
-    "key=d99803c970a04223998cabd90a741633" +
-    "&stop_id=it"
 
-request({
-    url: url,
-    json: true
-}, function (error, response, body) {
-
-    if (!error && response.statusCode === 200) {
-        console.log(body) // Print the json response
-    }
-})
 
 module.exports = router;
