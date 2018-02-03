957a2b7537001e22dceaaa3cc0ee96babbb7d3c9
Joey Tepperman <15jmt11@queensu.ca>
2018-02-03 13:38:01 -0500

    data

--- seed/hospital-seeder.js
+++ seed/hospital-seeder.js
@@ -1,12 +1,19 @@
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017/";
+var Hospital = require('../models/hospital');
+
+var testHospital = new Hospital({
+  name: 'Hospital',
+  inLine: 0,
+  onTheWay: 0
+});
 
 MongoClient.connect(url, function(err, db) {
   if (err) throw err;
   var dbo = db.db("hospitalData");
-  dbo.createCollection("hospitals", function(err, res) {
+  dbo.collection("hospitals").insertOne(testHospital, function(err, res) {
     if (err) throw err;
-    console.log("Collection created!");
+    console.log("1 document inserted");
     db.close();
   });
 });
