6eacd137c7a330634d99e00f739c09e51b765f09
Joey Tepperman <15jmt11@queensu.ca>
2018-02-04 02:45:02 -0500

    hello

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,16 @@
 var express = require('express');
 var router = express.Router();
 var request = require('request');
-
+var geocoder = require('google-geocoder');
+var geo = geocoder({
+  key: 'AIzaSyBiJKNdm1r_N0HUAo9YLvbNNirrHJh9DuQ'
+});
+var geodist = require('geodist')
 
 /* GET home page. */
 var MongoClient = require('mongodb').MongoClient;
 
-MongoClient.connect("mongodb://localhost:27017/test?ssl=true", function(err, db) {
-  db.close();
-});
+
 
   router.get('/', function(req, res, next) {
     res.render('index', { title: 'WaitingRoom' });
@@ -19,12 +21,53 @@ MongoClient.connect("mongodb://localhost:27017/test?ssl=true", function(err, db)
 
   router.post('/click', function(req, res) {
     var test = req.body.test
+    var lat, lng;
+    var results = [];
+    geo.find(test, function(err, res){
+      if(!err) {
+        console.log(res[0]);
+        try{
+          lat = res[0].location.lat;
+          lng = res[0].location.lng;
+        }catch(e){
+          lat = null;
+          lng = null;
+        }
+      }
+    });
 
+    MongoClient.connect("mongodb://localhost:27017/hospitalData", function(err, db) {
 
-    var url = "http://maps.googleapis.com/maps/api/geocode/json?address=hospital " + test;
-    console.log("url = "+url);
+      var dbo = db.db("hospitalData");
+      dbo.collection("hospitals").find({}).toArray(function(err, result){
+        if(err) throw err;
+        try{
+        console.log(result[0].name);
+        //res.send(JSON.stringify(result[0]));
+        }catch(e){}
 
-    request({
+        if(lat!=null & lng!=null){
+          for(var i = 0; i<result.length; i++){
+            var dist = geodist({lat: result[i].lat, lon: result[i].lng}, {lat: lat, lon: lng});
+            //console.log(dist);
+            results.push({
+              hospital: result[i],
+              distance: dist
+
+            });
+          }
+          res.send(
+            JSON.stringify(
+              results.sort(function(a, b){
+                return a.distance - b.distance;
+              })[0].hospital));
+
+        }
+
+        db.close();
+      });
+    });
+    /*request({
         url: url,
         json: true
     }, function (error, response, body) {
@@ -34,10 +77,11 @@ MongoClient.connect("mongodb://localhost:27017/test?ssl=true", function(err, db)
             console.log(results);
             //console.log(body.results) // Print the json response
         }
-    });
+    });*/
+
     console.log(test);
 
-    res.send(test);
+
   });
 
 
